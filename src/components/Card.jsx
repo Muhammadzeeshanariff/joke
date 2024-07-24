381d05d390ab576cbb226c66e4035cd6c6ccbb12
Muhammadzeeshanariff <muhammadzeeshanariff@gmail.com>
2024-07-24 14:25:27 +0500

    04/added card

---
+++ src/components/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>Card
+        <input type="text" name="" id="" />
+        <button>submit</button>
+
+    </div>
+  )
+}
+
+export default Card
