f17ca63fdbf5674ae35ae24ffdba46d878206955
Muhammadzeeshanariff <muhammadzeeshanariff@gmail.com>
2024-07-26 14:20:29 +0500

    02/updated the joke list

--- src/components/JokeList.jsx
+++ src/components/JokeList.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const JokeList = () => {
   return (
-    <div>JokeList</div>
-  )
-}
+    <div>
+      <ol>
+        <li>joke 1</li>
+        <li>joke 2</li>
+        <li>joke 3</li>
+        <li>joke 4</li>
+        <li>joke 5</li>
+      </ol>
+    </div>
+  );
+};
 
-export default JokeList
+export default JokeList;
