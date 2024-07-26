24da0ad1fab966492db8162b7b1990f4e85c790a
Muhammadzeeshanariff <muhammadzeeshanariff@gmail.com>
2024-07-26 14:29:25 +0500

    06/joke details added

---
+++ src/components/JokeDetails.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const JokeDetails = () => {
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam ipsum,
+        dolorem dolores iste libero accusantium! Placeat aperiam voluptates amet
+        veritatis aspernatur optio quam impedit nisi illum eum! Atque voluptate
+        tempore possimus voluptas ducimus explicabo vero perferendis id sed
+        maxime nam obcaecati, sint magnam optio consequatur nesciunt dolorum
+        mollitia assumenda facere!
+      </p>
+    </div>
+  );
+};
+
+export default JokeDetails;
