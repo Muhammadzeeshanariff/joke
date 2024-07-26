8fe46dada57f388197c163ccc680daff8bad05e6
Ehtasham656 <135942599+Ehtasham656@users.noreply.github.com>
2024-07-26 14:43:13 +0500

    08/Updated Joke Details File

--- src/components/JokeDetails.jsx
+++ src/components/JokeDetails.jsx
@@ -1,16 +1,7 @@
-import React from "react";
-
 const JokeDetails = () => {
   return (
     <div>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam ipsum,
-        dolorem dolores iste libero accusantium! Placeat aperiam voluptates amet
-        veritatis aspernatur optio quam impedit nisi illum eum! Atque voluptate
-        tempore possimus voluptas ducimus explicabo vero perferendis id sed
-        maxime nam obcaecati, sint magnam optio consequatur nesciunt dolorum
-        mollitia assumenda facere!
-      </p>
+      <p>Add something in English Buddy and focus on your work.</p>
     </div>
   );
 };
